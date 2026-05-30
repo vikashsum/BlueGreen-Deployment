ae555b5413c0b183b01f004b15c485bf9727c364
vikashsum <vikashsuman886@gmail.com>
2026-05-30 22:31:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Blue-Green Deployment App"
+    return "Blue-Green Deployment Running on EKS!"
 
 @app.route('/health')
 def health():
-    return {"status": "healthy"}, 200
+    return jsonify({
+        "status": "healthy"
+    }), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
