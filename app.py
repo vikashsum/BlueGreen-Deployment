d9b608dd33a63fe5d1fcceb7988a0d5837f9416b
vikashsum <vikashsuman886@gmail.com>
2026-05-30 22:19:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Blue-Green Deployment Running on EKS!"
+    return "Blue-Green Deployment App"
+
+@app.route('/health')
+def health():
+    return {"status": "healthy"}, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
